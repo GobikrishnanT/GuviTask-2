6bc5e690d972b143436fa76cb4832ea989a4b8ec
Gobikrishnan T <68734147+GobikrishnanT@users.noreply.github.com>
2021-03-28 18:16:57 +0530

    Update API.js

--- API.js
+++ API.js
@@ -1,55 +1,39 @@
-
-
-//1 . Create a instance of XMLHttpRequest();
-var request = new XMLHttpRequest();
-
-// 2 . Create a connection : //Open a connection
-request.open('GET' , 'https://restcountries.eu/rest/v2/all' , true) 
-//Get : Method , URL : link  , true : depend for 
-//free and escape from any issue
-
-// 3 . Send the request :
+let request = new XMLHttpRequest();
+request.open("GET" , "https://restcountries.eu/rest/v2/all" , true);
 request.send();
-
-//4 . Load the response ...This function is triggered only when the date
-// is retrived
-
 request.onload = function() {
-    var myData = JSON.parse(this.response);
-    console.log(myData);
+    var myRestCountry = JSON.parse(this.response);
+    console.log(myRestCountry);
 
-    for(let i = 0 ; i < myData.length ; i++) {
-        if(myData[i].latlng[0] > 0 && myData[i].latlng[1] > 0) {
-            latData = myData[i].latlng[0];
-            lngData = myData[i].latlng[1];
+    for(let index in myRestCountry) {
+        try{
+            var countryName = myRestCountry[index].name;
+            var [lat , lng] = myRestCountry[index].latlng;
+            if(lat === 0 && lng === 0) {
+                throw new Error("Oops country is restricted");
+            }
+            maginFunction(countryName , lat , lng);
         }
-       
-        //console.log(latData , lngData);
-        var request = new XMLHttpRequest();
-request.open('GET' , `http://api.openweathermap.org/data/2.5/weather?lat=${lngData}&lon=${latData}&appid=bfbf8184796f43fd1147e3fbaf3aaa67` , true);
-        request.send();
-
-       request.onload = function() {
-          var myCountryData = JSON.parse(this.response);
-          if(myCountryData["cod"] === 200) {
-            var lon = myCountryData["coord"]["lon"] || myCountryData["message"];
-            var lat = myCountryData["coord"]["lat"] || myCountryData["message"];
-            var nameOfCountry = myCountryData["name"] || "No Country Name";
-            console.log(`Latitude : ${lat} Longitude : ${lon} Countryname : ${nameOfCountry}`); 
-          }
-            
-          
-       }
-
-
-    }  
-    
+        catch(err) {
+            console.log("Invalid country credential" + countryName + " " + err.message);
+        }
+    }
 }
-
-    
-
-
-
-
-
-
+function maginFunction(cname , lat , lng) {
+    let key = "bfbf8184796f43fd1147e3fbaf3aaa67";
+    let URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`;
+    let newRequest = new XMLHttpRequest();
+    newRequest.open("GET" , URL , true);
+    newRequest.send();
+    newRequest.onload = function() {
+        let mainData = JSON.parse(this.response);
+        try{
+            
+            console.log(mainData.name + " " + mainData.main.temp);
+            
+        }
+        catch(err){
+            console.log(err.message);
+        }
+    }
+}
